e37bfa9fcf14868232fce97e12fa126567742fdf
Alexander Kulichkov <hetyh2004@gmail.com>
2024-12-01 00:03:35 +0100

    Changed logging

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,25 +1,21 @@
 import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
 
 import { Request, Response, NextFunction } from 'express';
-import { randomUUID } from 'node:crypto';
 
 @Injectable()
 export class AppLoggerMiddleware implements NestMiddleware {
   private logger = new Logger('HTTP', { timestamp: true });
 
   use(request: Request, response: Response, next: NextFunction): void {
-    const { ip, method, path: url } = request;
+    const { ip, method, path } = request;
     const userAgent = request.get('user-agent') || '';
-    const id = randomUUID();
-
-    this.logger.log(`START ${id} ${method} ${url} - ${userAgent} ${ip}`);
+    const startTime = Date.now();
 
     response.on('close', () => {
       const { statusCode } = response;
-      const contentLength = response.get('content-length');
-
+      const endTime = Date.now();
       this.logger.log(
-        `END ${id} ${method} ${url} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
+        `START ${startTime} END ${endTime} - ${method} ${path} ${statusCode} - ${userAgent} ${ip}`,
       );
     });
 
